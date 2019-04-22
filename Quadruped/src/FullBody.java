c58ff86bef1e23c08f87f7b15f913b21672533d2
Megan-Choy <43397124+Megan-Choy@users.noreply.github.com>
2019-04-21 21:27:24 -0700

    created constructors with images

--- Quadruped/src/FullBody.java
+++ Quadruped/src/FullBody.java
@@ -1,6 +1,24 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
-public class FullBody{
+public class FullBody {
+	private Image bodyImage;
+	private Image legTL, legTR, legBL, legBR;
 	
-	Body body = new Body();
-	Legs legs = new Legs();
+	public FullBody() {
+		
+		bodyImage = (new ImageIcon("body.jpg")).getImage();
+		legTL = (new ImageIcon("leg1.jpg")).getImage();
+		legTR = (new ImageIcon("leg2.jpg")).getImage();
+		legBL = (new ImageIcon("leg3.jpg")).getImage();
+		legBR = (new ImageIcon("leg4.jpg")).getImage();
+		
+		Body body = new Body(bodyImage);
+		Legs legs = new Legs(legTL, legTR, legBL, legBR);
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
 }
--- Quadruped/src/Legs.java
+++ Quadruped/src/Legs.java
@@ -1,4 +1,15 @@
+import java.awt.Image;
 
-public class Legs extends Body{
+public class Legs {
+	private Image legTL, legTR, legBL, legBR; // Top left, top right, back left, back right
+	
+	public Legs(Image legTL, Image legTR, Image legBL, Image legBR) {
+		this.legTL = legTL;
+		this.legTR = legTR;
+		this.legBL = legBL;
+		this.legBR = legBR;
+
+	}
+
 
 }
