3119c0353e1cf5c5b2b64cf473567671f7c1871e
Laksh Bhambhani <lakshbh@gmail.com>
2019-04-28 18:02:40 -0700

    Update pushUpTest.py

--- Quadruped-Raspberry Pi/Quadruped/pushUpTest.py
+++ Quadruped-Raspberry Pi/Quadruped/pushUpTest.py
@@ -3,13 +3,6 @@ import time
 
 quad = Quadruped.Quadruped()
 
-try:
-    for x in range(5):
-         quad.bow()
-         time.sleep(1)
-         quad.bend()     
-     
-     
-except:
-    quad.homePos()
-
+quad.bow()
+time.sleep(1)
+quad.bend()     
