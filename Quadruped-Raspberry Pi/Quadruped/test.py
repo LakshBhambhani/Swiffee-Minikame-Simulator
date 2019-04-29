7dc508880ff6b7378ecc8e8c775cb5431da56a70
Laksh Bhambhani <lakshbh@gmail.com>
2019-04-28 17:58:40 -0700

    Update test.py

--- Quadruped-Raspberry Pi/Quadruped/test.py
+++ Quadruped-Raspberry Pi/Quadruped/test.py
@@ -9,6 +9,13 @@ try:
     time.sleep(1)
     for x in range(6):
         quad.turnRight()
+    time.sleep(1)
+    for x in range(6):
+        quad.walkForward()
+    for x in range(6):
+        quad.bow()
+        time.sleep(1)
+        quad.bendBack()
         
 except:
     quad.homePos()
