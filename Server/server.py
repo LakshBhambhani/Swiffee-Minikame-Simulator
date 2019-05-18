a16d81d9a21c38880ec633a825bb833194b5b25c
Laksh Bhambhani <lakshbh@gmail.com>
2019-05-18 14:23:17 -0700

    Server updated to return aknowledged requests

--- Server/server.py
+++ Server/server.py
@@ -17,19 +17,33 @@ def main():
       }
    # Pass the template data into the template main.html and return it to the user
    return message
-
+  
 def walkForward():
    quad.walkForward()
 
-@app.route('/status')
-def foo():
-   #t1 = threading.Thread(target=walkForward(), args=()) 
-   #t1.start()
-   return message + 'start'
-  
-def walkForward():
-        # do something that takes a long time
-        quad.walkForward()
+def turnLeft():
+   quad.turnLeft()
+
+def turnRight():
+   quad.turnRight()
+
+def homePos():
+   quad.homePos()
+
+def bow():
+   quad.bow()
+
+def bendBack():
+   quad.bendBack()
+
+def jumpUp():
+   quad.jumpUp()
+
+def jumpBack():
+   quad.jumpBack()
+
+def pushUp():
+   quad.pushUp()
 
 @app.route("/<action1>")
 def action1(action1):
@@ -37,40 +51,51 @@ def action1(action1):
    global message
    if action1 == "walkForward":
       message = "Walking Forward"
-      #return redirect(url_for('foo'))
       thread = threading.Thread(target=walkForward)
       thread.start()
-      return 'started'   
+      return message + ' started'   
    elif action1 == "turnLeft":
       message = "Turning Left"
-      quad.turnLeft()
+      thread = threading.Thread(target=turnLeft)
+      thread.start()
+      return message + ' started' 
    elif action1 == "turnRight":
       message = "Turning Right"
-      quad.turnRight()
+      thread = threading.Thread(target=turnRight)
+      thread.start()
+      return message + ' started' 
    elif action1 == "homePos":
       message = "Switching to Home Position"
-      quad.homePos()
+      thread = threading.Thread(target=homePos)
+      thread.start()
+      return message + ' started' 
    elif action1 == "bow":
       message = "Bowing"
-      quad.bow()
+      thread = threading.Thread(target=bow)
+      thread.start()
+      return message + ' started' 
    elif action1 == "bendBack":
       message = "Bending Back"
-      quad.bendBack()
+      thread = threading.Thread(target=bendBack)
+      thread.start()
+      return message + ' started' 
    elif action1 == "jumpUp":
       message = "Jumping Up"
-      quad.jumpUp()
+      thread = threading.Thread(target=jumpUp)
+      thread.start()
+      return message + ' started' 
    elif action1 ==  "jumpBack":
       message = "Jumping Back"
-      quad.jumpBack()
+      thread = threading.Thread(target=jumpBack)
+      thread.start()
+      return message + ' started' 
    elif action1 == "pushUp":
       message = "Completed 5 push ups"
-      quad.pushUp()
+      thread = threading.Thread(target=pushUp)
+      thread.start()
+      return message + ' started' 
    else:
       message = ""
-   
-   if message == "Walking Forward":
-      return message()
-      quad.walkForward()
 
    templateData = {
       'message' : message,
