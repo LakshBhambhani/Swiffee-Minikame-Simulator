aa5dcc285b8730384c370b5fc2c9a35c611598e7
Laksh Bhambhani <lakshbh@gmail.com>
2019-05-22 10:58:56 -0700

    Update Quadruped.py

--- Server/Quadruped.py
+++ Server/Quadruped.py
@@ -305,19 +305,19 @@ class Quadruped():
             kit.servo[5].angle = 150    #FL FOOT
             kit.servo[11].angle = 30   #BR FOOT
 
-            time.sleep(0.2)
+            time.sleep(0.8)
 
             kit.servo[7].angle = 50    #FL FOOT
             kit.servo[9].angle = 60   #BR FOOT
             kit.servo[5].angle = 110    #FL FOOT
             kit.servo[11].angle = 110   #BR FOOT
 
-            time.sleep(0.2)
+            time.sleep(0.8)
 
             kit.servo[7].angle = 20    #FL FOOT
             kit.servo[9].angle = 120   #BR FOOT
 
-            time.sleep(0.2)
+            time.sleep(0.8)
 
             kit.servo[7].angle = 70    #FL FOOT
             kit.servo[9].angle = 70   #BR FOOT
