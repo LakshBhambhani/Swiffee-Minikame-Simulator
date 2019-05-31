c6be98dc5a39c3d6ce9f26732379704009a94c04
Laksh Bhambhani <lakshbh@gmail.com>
2019-05-30 21:47:00 -0700

    Update Quadruped.py

--- Server/Quadruped.py
+++ Server/Quadruped.py
@@ -372,8 +372,8 @@ class Quadruped():
         self.homePos()        
         kit.servo[7].angle = 0     #FR FOOT
         for x in range(5):
-            kit.servo[6].angle = 100
-            time.sleep(0.3)
+            kit.servo[6].angle = 80
+            time.sleep(0.5)
             kit.servo[6].angle = 150
 
 
